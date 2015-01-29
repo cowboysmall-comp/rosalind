a0b1bd37c3e531f4f94a8752d17737bf765b1a4b
Jerry Kiely <jerry@cowboysmall.com>
2015-01-29 01:14:59 -0500

    made some further enhancements to cat

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# rosalind
+rosalind
+========
+
 Rosalind is a platform for learning bioinformatics and programming through problem solving.
--- src/stronghold/rosalind_cat.py
+++ src/stronghold/rosalind_cat.py
@@ -1,32 +1,104 @@
 import sys
 
-import numpy as np
 
 """
-    Just out of interest, here is a version, based on a version 
-    written in Ruby that I found in the forums, and translated 
-    into Python. It is much slower, but I find it fascinating. 
-    For some reason, examples of Dynamic Programming always tend 
-    to turn my brain inside out, and this example is no different 
-    in that respect.
+    Here are some earlier (and alternative) versions of the matcher:
 
-    def matchings(rna):
-        A = np.zeros((len(rna), len(rna)), dtype = int)
+    1) this was the original. It's a little nasty, but pretty 
+    simple. Perhaps the start and end indexes are a little 
+    confusing - especially around the recursive calls. I decided 
+    I needed to at least think about making it a little easier 
+    on the eyes and brain.
 
-        for i in xrange(len(rna)):
-            for j in xrange(i - 1, -1, -1):
-                if rna[i] == COMPLEMENT[rna[j]]:
-                    if i - 1 >= j + 1:
-                        A[i, j] = A[i - 1, j + 1]
-                    else:
-                        A[i, j] = 1
+        def matchings(rna, start, end, A):
+            if end < start:
+                return 1
+            elif A[start, end] == -1:
+                A[start, end] = 0
 
-            for j in xrange(i):
-                for k in xrange(j):
-                    A[i, k] += A[i, j] * A[j - 1, k]
-                    A[i, k] %= 1000000
+                if check_occurences(rna[start:end + 1]):
+                    for i in xrange(start + 1, end + 1, 2):
+                        if rna[start] == COMPLEMENT[rna[i]]:
+                            A[start, end] += matchings(rna, start + 1, i - 1, A) * matchings(rna, i + 1, end, A)
+                            A[start, end] %= 1000000
 
-        return A
+            return A[start, end]
+
+
+    2) here is a slightly altered version of 1 above - it is 
+    constructed to hide the creation of the numpy array - called 
+    A - used for sub-problem caching. The recursion step is also 
+    hidden from view. It's still not perfect, but a step in the 
+    right direction maybe. 
+
+        import numpy as np
+
+        def matchings(rna):
+            N = len(rna)
+            A = np.empty((N, N), dtype = int)
+            A.fill(-1)
+
+            def match(rna, start, end):
+                if end < start:
+                    return 1
+                elif A[start, end] == -1:
+                    A[start, end] = 0
+                    if check_occurences(rna[start:end + 1]):
+                        for i in xrange(start + 1, end + 1, 2):
+                            if rna[start] == COMPLEMENT[rna[i]]:
+                                A[start, end] += match(rna, start + 1, i - 1) * match(rna, i + 1, end)
+                                A[start, end] %= 1000000
+                return A[start, end]
+
+            return match(rna, 0, N - 1)
+
+
+    3) this was a version I wrote soon after - it seemed cleaner 
+    to me because it used split strings, and no indexes were passed
+    around. Also, it used a Python dictionary, rather than a numpy 
+    array (not that I object to numpy in the slightest - on the 
+    contrary, I love it - but I set myself the task of using standard 
+    Python as much as possible on these solutions). The final version 
+    is the one used below - it's same as this version, but as above 
+    the sub-problem cache and recursion steps are now hidden.
+
+        def matchings(rna, A):
+            if len(rna) == 0:
+                return 1
+            if rna not in A:
+                A[rna] = 0
+                if check_occurences(rna):
+                    for i in xrange(1, len(rna), 2):
+                        if rna[0] == COMPLEMENT[rna[i]]:
+                            A[rna] += matchings(rna[1:i], A) * matchings(rna[i + 1:], A)
+                            A[rna] %= 1000000
+            return A[rna]
+
+
+    4) and just out of interest, here is a version, based on a 
+    version written in Ruby that I found in the forums, and 
+    translated into Python. It is much slower, but I find it 
+    fascinating. For some reason, examples of Dynamic Programming 
+    always tend to turn my brain inside out, and this example is no 
+    different in that respect.
+
+        def matchings(rna):
+            A = np.zeros((len(rna), len(rna)), dtype = int)
+
+            for i in xrange(len(rna)):
+                for j in xrange(i - 1, -1, -1):
+                    if rna[i] == COMPLEMENT[rna[j]]:
+                        A[i, j] = A[i - 1, j + 1] if j + 1 <= i - 1 else 1
+
+                for j in xrange(i):
+                    for k in xrange(j):
+                        A[i, k] += A[i, j] * A[j - 1, k]
+                        A[i, k] %= 1000000
+
+            return A
+
+
+    I have no idea what's going on here :-)
 
 """
 
@@ -48,29 +120,24 @@ def check_occurences(rna):
     return rna.count('A') == rna.count('U') and rna.count('C') == rna.count('G')
 
 
-def matchings(rna, start, end, A):
-    if end < start:
-        return 1
-    elif A[start, end] == -1:
-        A[start, end] = 0
-
-        if check_occurences(rna[start:end + 1]):
-            for i in xrange(start + 1, end + 1, 2):
-                if rna[start] == COMPLEMENT[rna[i]]:
-                    A[start, end] += matchings(rna, start + 1, i - 1, A) * matchings(rna, i + 1, end, A)
-                    A[start, end] %= 1000000
-
-    return A[start, end]
+def matchings(rna):
+    A = {}
+    def match(rna):
+        if len(rna) == 0:
+            return 1
+        if rna not in A:
+            A[rna] = 0
+            if check_occurences(rna):
+                for i in xrange(1, len(rna), 2):
+                    if rna[0] == COMPLEMENT[rna[i]]:
+                        A[rna] += match(rna[1:i]) * match(rna[i + 1:])
+                        A[rna] %= 1000000
+        return A[rna]
+    return match(rna)
 
 
 def main(argv):
-    rna = read_fasta(argv[0])
-    N   = len(rna)
-
-    A = np.empty((N, N), dtype = int)
-    A.fill(-1)
-
-    print matchings(rna, 0, N - 1, A)
+    print matchings(read_fasta(argv[0]))
 
 
 if __name__ == "__main__":
