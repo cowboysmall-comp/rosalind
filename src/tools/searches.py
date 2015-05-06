1a40682c7a3087929eef4d270b8904b66b4b177f
Jerry Kiely <jerry@cowboysmall.com>
2015-05-06 14:26:04 -0400

    slight change to implementation of binary search

--- src/tools/searches.py
+++ src/tools/searches.py
@@ -1,15 +1,56 @@
 
 
-def binary_search(array, key, start, end):
-    if end < start:
+'''
+    some other implementations:
+
+
+    recursive:
+
+    def binary_search(array, key, start, end):
+        if end < start:
+            return -1
+        else:
+            midpoint = start + (end - start) // 2
+
+            if key < array[midpoint]:
+                return binary_search(array, key, start, midpoint - 1)
+            elif key > array[midpoint]:
+                return binary_search(array, key, midpoint + 1, end)
+            else:
+                return midpoint
+
+
+    iterative:
+
+    def binary_search(array, key, start, end):
+        while start <= end:
+            midpoint = start + (end - start) // 2
+
+            if key < array[midpoint]:
+                end   = midpoint - 1
+            elif key > array[midpoint]:
+                start = midpoint + 1
+            else:
+                return midpoint
+
         return -1
-    else:
+
+'''
+
+
+
+
+def binary_search(array, key, start, end):
+    while start < end:
         midpoint = start + (end - start) // 2
 
-        if key < array[midpoint]:
-            return binary_search(array, key, start, midpoint - 1)
-        elif key > array[midpoint]:
-            return binary_search(array, key, midpoint + 1, end)
+        if array[midpoint] < key:
+            start = midpoint + 1
         else:
-            return midpoint
+            end   = midpoint
+
+    if start == end and array[start] == key:
+        return start
+    else:
+        return -1
 
